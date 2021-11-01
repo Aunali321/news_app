18be390df2b0bce5f1475a92a84f224b184fe589
AunAli <aunvakil.aa@gmail.com>
2021-11-01 14:22:01 +0530

    Working on searchbar

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -7,28 +7,55 @@ class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 40,
-      decoration: BoxDecoration(
-        color: Colors.grey[300],
-        borderRadius: BorderRadius.circular(10),
-      ),
+      // height: 40,
+      // decoration: BoxDecoration(
+      //   color: Colors.grey[300],
+      //   borderRadius: BorderRadius.circular(10),
+      // ),
       child: Row(
         children: [
-          MaterialButton(
-            onPressed: () {},
-            child: Image.asset(
-              "assets/images/search.png",
-              scale: 1.3,
-              color: const Color(0x95000000),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.grey[300],
+                filled: true,
+                hintText: "Search",
+                hintStyle: TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w400,
+                ),
+                prefix: Padding(
+                  padding: const EdgeInsets.only(right: 12.0),
+                  child: Image.asset(
+                    "assets/images/search.png",
+                    alignment: Alignment.bottomLeft,
+                    scale: 1.3,
+                    color: const Color(0x95000000),
+                    height: 28,
+                  ),
+                ),
+                contentPadding: EdgeInsets.only(left: 18),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none),
+              ),
             ),
-            minWidth: 10,
           ),
-          const Text(
-            "Search",
-            style: TextStyle(
-              color: Color(0x95000000),
-            ),
-          ),
+          // MaterialButton(
+          //   onPressed: () {},
+          //   child: Image.asset(
+          //     "assets/images/search.png",
+          //     scale: 1.3,
+          //     color: const Color(0x95000000),
+          //   ),
+          //   minWidth: 10,
+          // ),
+          // const Text(
+          //   "Search",
+          //   style: TextStyle(
+          //     color: Color(0x95000000),
+          //   ),
+          // ),
         ],
       ),
     );
